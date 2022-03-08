78addcbfd10b4bb7cd10e3179e18fb986581d29c
betalantz <betalantz@gmail.com>
2022-03-08 13:20:53 -0800

    completes deliv 3

--- src/components/AddTrackForm.jsx
+++ src/components/AddTrackForm.jsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function AddTrackForm() {
+function AddTrackForm({ onSubmitTrack}) {
+
+  const [formData, setFormData] = useState({
+    image: '',
+    title: '',
+    artist: '',
+    bpm: ''
+  })
+
+  const handleChange = e => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    onSubmitTrack(formData)
+    setFormData({
+      image: '',
+      title: '',
+      artist: '',
+      bpm: ''
+    })
+  }
   return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          <input type="text" name="image" placeholder="Image URL"/>
-          <input type="text" name="title" placeholder="title" />
-          <input type="text" name="artist" placeholder="Artist" />
-          <input type="number" name="bpm" placeholder="BPM" step="1.00" />
+          <input onChange={handleChange} value={formData.image} type="text" name="image" placeholder="Image URL"/>
+          <input onChange={handleChange} value={formData.title} type="text" name="title" placeholder="title" />
+          <input onChange={handleChange} value={formData.artist} type="text" name="artist" placeholder="Artist" />
+          <input onChange={handleChange} value={formData.bpm} type="number" name="bpm" placeholder="BPM" step="1.00" />
        </div>
         <input className="" type="submit" value="Add Track" />
       </form>
--- src/components/TracksPage.jsx
+++ src/components/TracksPage.jsx
@@ -5,14 +5,42 @@ import TracksList from './TracksList'
 
 function TracksPage() {
 
+  const baseUrl = "http://localhost:8001/tracks"
+
   const [tracks, setTracks] = useState([])
   const [searchTerm, setSearchTerm] = useState("")    
 
   useEffect(() => {
-    fetch("http://localhost:8001/tracks")
+    fetch(baseUrl)
       .then(res => res.json())
       .then(setTracks)
   }, [])
+
+  const postNewTrack = ({image, title, artist, bpm}) => {
+    const newTrackBody = {
+      image,
+      title,
+      artist,
+      BPM: Number(bpm)
+    }
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newTrackBody)
+    }
+    return fetch(baseUrl, options)
+      .then(res => res.json())
+  }
+
+  const addTrack = newTrack => {
+    console.log('newTrack: ', newTrack);
+    postNewTrack(newTrack)
+      .then(newTrack => {
+        setTracks([...tracks, newTrack])
+      })
+  }
 
   const filteredTracks = tracks.filter(track => {
     return track.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
@@ -23,7 +51,7 @@ function TracksPage() {
   return (
     <div>
       <Search searchTerm={searchTerm} onSearchChange={setSearchTerm}/>
-      <AddTrackForm />
+      <AddTrackForm onSubmitTrack={addTrack}/>
       <TracksList tracks={filteredTracks}/>
     </div>
   )
